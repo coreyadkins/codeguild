3bff1bc6a9b4b159ea7181a69d88011753e13a04
Corey Adkins <adkinsbass@gmail.com>
2016-07-08 12:26:46 -0700

    Changed for style

--- practice/wall-painting.py
+++ practice/wall-painting.py
@@ -1,7 +1,8 @@
 """This program will determine the cost of painting a wall"""
 #1. Setup
 GALLON_COVERAGE = 400 #coverage in Sq. Feet
-job_cost = 0
+JOB_COST = 0
+SUM_AREA = 0
 
 
 #2. Input
@@ -19,21 +20,20 @@ while num_walls >= 1:
     wall_height = int(input())
     print('How many coats would you like to put on your wall:')
     num_coats = int(input())
-    wall_area = wall_width * wall_height
-    gallons_paint = wall_area / GALLON_COVERAGE
-    gallons_paint *= num_coats
-    wall_cost = float(gallons_paint) * paint_cost
-    job_cost += wall_cost
+    SUM_AREA = (wall_width * wall_height) * num_coats #Outputs units in Sq. Feet
     num_walls -= 1
 
 #3. Transform
 
 import math
 
-job_cost = math.ceil(job_cost)
+gallons_paint = SUM_AREA / GALLON_COVERAGE
+wall_cost = float(gallons_paint) * paint_cost
+JOB_COST += wall_cost
+JOB_COST = math.ceil(JOB_COST)
 
 #4. Output
-if job_cost <= 1:
-    print('This job will cost ' + str(job_cost) + ' dollar')
+if JOB_COST <= 1:
+    print('This job will cost ' + str(JOB_COST) + ' dollar')
 else:
-    print('This job will cost ' + str(job_cost) + ' dollars')
+    print('This job will cost ' + str(JOB_COST) + ' dollars')
