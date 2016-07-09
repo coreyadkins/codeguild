96284418d3313833ac96ae5be840e5c1b3f7a7ad
Corey Adkins <adkinsbass@gmail.com>
2016-07-09 12:12:55 -0700

    Final version

--- practice/wall-painting.py
+++ practice/wall-painting.py
@@ -2,8 +2,8 @@
 #1. Setup
 GALLON_COVERAGE = 400 #coverage in Sq. Feet
 JOB_COST = 0
-AREA_LIST = [0]
-SUM_AREA = 0
+AREA_LIST = []
+SUM_ALL_AREAS = 0
 
 
 #2. Input
@@ -14,33 +14,32 @@ print('Cost of a gallon of paint, in dollars:')
 paint_cost = float(input())
 
 
-while num_walls >= 1: #Need to create additive variable which ouputs list that is totalled in SUM_AREA, rather than using SUM_AREA in while loop
+while num_walls >= 1: #Need to create additive variable which ouputs list that is totalled in SUM_ALL_AREAS, rather than using SUM_ALL_AREAS in while loop
     print('Width of the wall in feet:')
     wall_width = int(input())
     print('Height of the wall in feet:')
     wall_height = int(input())
     print('How many coats would you like to put on your wall:')
     num_coats = int(input())
-    area = (wall_height * wall_width) * num_coats #Outputs units in Sq. Feet
-    print(area)
-    AREA_LIST += str(area)
+    wall_area = (wall_height * wall_width) * num_coats #Outputs units in Sq. Feet
+    AREA_LIST += [wall_area]
     num_walls -= 1
-    print(AREA_LIST)
 
 #3. Transform
 
 import math
 
 
-SUM_AREA = sum(AREA_LIST)
-SUM_AREA = (wall_width * wall_height) * num_coats
-gallons_paint = SUM_AREA / GALLON_COVERAGE
-gallons_purchased = math.ceil(gallons_paint)
-wall_cost = float(gallons_purchased) * paint_cost
+SUM_ALL_AREAS = sum(AREA_LIST)
+gallons_required = SUM_ALL_AREAS / GALLON_COVERAGE
+gallons_to_purchase = math.ceil(gallons_required)
+wall_cost = float(gallons_to_purchase) * paint_cost
 JOB_COST += wall_cost
 
 #4. Output
-if JOB_COST <= 1:
-    print('This job will cost ' + str(round(JOB_COST)) + ' dollar')
+if JOB_COST == 1:
+    print('This job will require ' + str(gallons_to_purchase) +
+        ' gallons of paint and cost ' + str(round(JOB_COST)) + ' dollar')
 else:
-    print('This job will cost ' + str(round(JOB_COST)) + ' dollars')
+    print('This job will require ' + str(gallons_to_purchase) +
+        ' gallons of paint and cost ' + str(round(JOB_COST)) + ' dollars')
