3ee4996abb0986bd8688d89a84f7aec397652db4
Corey Adkins <adkinsbass@gmail.com>
2016-07-11 21:18:19 -0700

    Updated 21 program with suggestion

--- practice/21.py
+++ practice/21.py
@@ -13,15 +13,16 @@ second_card = input('What is your second card?')
 
 if first_card in NAME_CARDS:
     first_card_value = 10
-if first_card in NUMBER_CARDS:
+elif first_card in NUMBER_CARDS:
     first_card_value = int(first_card)
-if first_card == 'A':
+elif first_card == 'A':
     first_card_value = 1
+
 if second_card in NAME_CARDS:
     second_card_value = 10
-if second_card in NUMBER_CARDS:
+elif second_card in NUMBER_CARDS:
     second_card_value = int(second_card)
-if second_card == 'A':
+elif second_card == 'A':
     second_card_value = 1
 
 sum_card_values = first_card_value + second_card_value
@@ -31,7 +32,7 @@ if first_card == 'A' or second_card == 'A' and sum_card_values <= 11:
 # Determining Outputs (Note: Add functionality to input hit card)
 if sum_card_values < 17:
     output = 'Hit!'
-if sum_card_values >=17:
+if sum_card_values >= 17:
     output = 'Stay!'
 if sum_card_values == 21:
     output = 'Blackjack!'
