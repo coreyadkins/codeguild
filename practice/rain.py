32764f5301c572b4f8b3da5fb81a59d97ee1923f
Corey Adkins <adkinsbass@gmail.com>
2016-07-21 09:40:34 -0700

    Incorporating feedback

--- practice/rain.py
+++ practice/rain.py
@@ -3,6 +3,7 @@ most rain, and the year with the most rain.
 """
 # 1. Setup
 import operator
+from itertools import groupby
 
 
 # 2. Define
@@ -14,6 +15,7 @@ def import_table():
     stripped_table = strip_table(intial_table)
     return cut_out_items(stripped_table)
 
+
 def strip_table(table):
     r"""Strips the table of formatting.
 
@@ -21,6 +23,7 @@ def strip_table(table):
     [['A']]
     """
     return [line.strip().split() for line in table[12:]]
+
 
 def cut_out_items(stripped_table):
     """Cuts out unused parts of the imported data.
@@ -30,14 +33,16 @@ def cut_out_items(stripped_table):
     """
     return [line[:2] for line in stripped_table]
 
+
 def get_date_to_amt(table):
     """Turns data into a dictionary that pairs each date with the rainfall amount on that day.
 
     >>> get_date_to_amt([['A', '3']])
     {'A': 3}
     """
-    date_to_amt = {key: value for (key, value) in table}
+    date_to_amt = dict(table)
     return filter_table(date_to_amt)
+
 
 def find_date_with_most_rain(date_to_amt):
     """Compares all dictionary items and returns the date with the highest value.
@@ -47,27 +52,34 @@ def find_date_with_most_rain(date_to_amt):
     """
     return max(date_to_amt.items(), key=operator.itemgetter(1))[0]
 
+
 def filter_table(date_to_amt):
     """Removes dates which have no data from data set.
 
     >>> filter_table({'15-DEC-2010': '-', '24-JUL-2009': '0'})
     {'24-JUL-2009': 0}
     """
-    return {date: int(amt) for date, amt in date_to_amt.items() if not '-' == amt}
+    return {date: int(amt) for date, amt in date_to_amt.items() if amt in '-'}
+
 
 def sort_table_by_year(date_to_amt):
     """Groups all values into new items based on the year of the data.
 
-    >>> [(key, sorted(list_val)) for key, list_val in sorted(sort_table_by_year({'15-NOV-2009': 6, '08-JAN-2009': 0}).items())]
+    >>> [(key, sorted(list_val)) for key, list_val in sorted(sort_table_by_year(
+    ... {'15-NOV-2009': 6, '08-JAN-2009': 0}).items())]
     [('2009', [0, 6])]
     """
-    year_to_amts= {}
+
+
+    year_to_amts = {}
     for date, amt in date_to_amt.items():
-        year = date[-4:]
-        if year not in year_to_amts:
-            year_to_amts[year] = []
-        year_to_amts[year] += [amt]
-    return year_to_amts
+        group_key
+    #     year = date[-4:]
+    #     if year not in year_to_amts:
+    #         year_to_amts[year] = []
+    #     year_to_amts[year] += [amt]
+    # return year_to_amts
+
 
 def find_year_with_most_rain(year_to_amts):
     """Compares all items and returns the year with the highest value.
@@ -77,6 +89,7 @@ def find_year_with_most_rain(year_to_amts):
     """
     total_rain_by_year = sum_yearly_amounts(year_to_amts)
     return max(total_rain_by_year.items(), key=operator.itemgetter(1))[0]
+
 
 def sum_yearly_amounts(year_to_amts):
     """Maps the dictionary and sums the values of all items.
@@ -88,6 +101,7 @@ def sum_yearly_amounts(year_to_amts):
     return year_to_amts
 
 # 3. Main
+
 
 def main():
     table = import_table()
