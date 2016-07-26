49a6f3339097241c4284377e5aa960bb65b48b55
Corey Adkins <adkinsbass@gmail.com>
2016-07-26 10:21:30 -0700

    List_board functional

--- practice/tic-tac-toe/list_board.py
+++ practice/tic-tac-toe/list_board.py
@@ -1,8 +1,9 @@
 class ListTTTBoard:
     def __init__(self):
         """Defines input value."""
-        self._rows = [['', '', ''], ['', '', ''], ['', '', '']]
+        self._rows = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
         self._columns = [[], [], []]
+        self._diags = [[], []]
 
     def __repr__(self):
         """Returns real version.
@@ -30,74 +31,100 @@ class ListTTTBoard:
         >>> X = ListTTTBoard()
         >>> X.place_token(1, 1, 'X')
         >>> X._rows
-        [['', '', ''], ['', 'X', ''], ['', '', '']]
+        [[' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' ']]
         """
         self._rows[y][x] = token
-
-    def table_as_columns(self):
-        """Returns table sorted into rows as table sorted into columns.
-
-        >>> X = ListTTTBoard()
-        >>> X._rows = [['X', 'O', ''], ['X', 'X', 'O'], ['X', 'O', '']]
-        >>> X.table_as_columns()
-        >>> X._columns
-        [['X', 'X', 'X'], ['O', 'X', 'O'], ['', 'O', '']]
-        """
-        for row in self._rows:
-            for item in range(len(row)):
-                self._columns[item].append(row[item])
 
     def calc_winner(self):
         """Calculates what token character string has won or None if no one has.
 
         >>> X = ListTTTBoard()
-        >>> X._rows = [['O', 'O', 'O'], ['X', 'X', 'O'], ['X', 'O', '']]
+        >>> X._rows = [['O', 'O', 'O'], ['X', 'X', 'O'], ['X', 'O', ' ']]
         >>> X.calc_winner()
         'O'
         >>> X = ListTTTBoard()
-        >>> X._rows = [['X', 'O', ''], ['X', 'X', 'O'], ['X', 'O', '']]
+        >>> X._rows = [['X', 'O', ' '], ['X', 'X', 'O'], ['X', 'O', ' ']]
         >>> X.calc_winner()
         'X'
-
         >>> X = ListTTTBoard()
-        >>> X._rows = [['Y', 'O', ''], ['X', 'Y', 'O'], ['', 'O', 'Y']]
+        >>> X._rows = [['Y', 'O', ' '], ['X', 'Y', 'O'], [' ', 'O', 'Y']]
         >>> X.calc_winner()
         'Y'
+        >>> X = ListTTTBoard()
+        >>> X._rows = [[' ', ' ', ' '], ['X', ' ', 'O'], [' ', 'O', 'X']]
+        >>> X.calc_winner() is not None
+        True
         """
         winner = None
-        for y in self._rows:
-            if len(set(y)) <= 1:
-                winner = y[0]
-            if winner == '':
+        for row in self._rows:
+            if len(set(row)) <= 1:
+                winner = row[0]
+            if winner == ' ':
                 winner = None
-            if winner != None:
+            if winner is not None:
                 return winner
             else:
                 winner = None
         self.table_as_columns()
-        for x in self._columns:
-            for y in x:
-                if len(set(y)) <= 1:
-                    winner = y
-                else:
-                    winner = None
-            if winner == '':
+        for column in self._columns:
+            if len(set(column)) <= 1:
+                winner = column[0]
+            else:
                 winner = None
-            if winner != None:
-                return winner
-        if len(set(self._rows[0][0] and self._rows[1][1] and
-                   self._rows[2][2])) <= 1 or len(set(self._rows[2][0] and self._rows[1][1] and self._rows[0][2])) <= 1:
-            winner = self._rows[1][1]
-            if winner == '':
+            if winner == ' ':
                 winner = None
-            if winner != None:
+            if winner is not None:
                 return winner
-        else:
-            winner = None
+        self.table_as_diags()
+        for diag in self._diags:
+            if len(set(diag)) <= 1:
+                winner = diag[0]
+            if winner == ' ':
+                winner = None
+            if winner is not None:
+                return winner
         return winner
 
-
-    def __str__():
+    def __str__(self):
         """Returns a pretty-printed picture of the board.
 
+        >>> X = ListTTTBoard()
+        >>> X._rows = [['O', 'O', 'O'], ['X', 'X', 'O'], ['X', 'O', ' ']]
+        >>> X.__str__()
+        O|O|O
+        X|X|O
+        X|O|
         """
+        for row in self._rows:
+            joined_row = '|'.join(row)
+            print(joined_row)
+
+    def table_as_columns(self):
+        """Returns table sorted into rows as table sorted into columns.
+
+        >>> X = ListTTTBoard()
+        >>> X._rows = [['X', 'O', ' '], ['X', 'X', 'O'], ['X', 'O', ' ']]
+        >>> X.table_as_columns()
+        >>> X._columns
+        [['X', 'X', 'X'], ['O', 'X', 'O'], [' ', 'O', ' ']]
+        """
+        for row in self._rows:
+            for item in range(len(row)):
+                self._columns[item].append(row[item])
+
+    def table_as_diags(self):
+        """Creates two lists which represent the diaganol axis on the TTT Board.
+
+        >>> X = ListTTTBoard()
+        >>> X._rows = [['Y', 'O', ' '], ['X', 'Y', 'O'], [' ', 'O', 'Y']]
+        >>> X.table_as_diags()
+        >>> X._diags
+        [['Y', 'Y', 'Y'], [' ', 'Y', ' ']]
+        """
+        self._diags[0].append(self._rows[0][0])
+        self._diags[0].append(self._rows[1][1])
+        self._diags[0].append(self._rows[2][2])
+        self._diags[1].append(self._rows[0][2])
+        self._diags[1].append(self._rows[1][1])
+        self._diags[1].append(self._rows[2][0])
+li
