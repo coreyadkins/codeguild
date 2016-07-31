e306014dea3228045e250b6430c4df3a1f07e607
Corey Adkins <adkinsbass@gmail.com>
2016-07-31 16:05:13 -0700

    Work continued

--- practice/tic-tac-toe/coords_board.py
+++ practice/tic-tac-toe/coords_board.py
@@ -1,7 +1,13 @@
 """This module performs the functions for Tic-Tac-Toe (TTT) program of placing a token on the board, determining the
 winner of a game, and returning a str version of the board, using tuples data type.
 """
-from collections import namedtuple
+from operator import itemgetter
+
+WINNING_COMBINATIONS = [[(0, 0), (1, 0), (2, 0)], [(0, 0), (0, 1), (0, 2)], [(1, 0), (1, 1), (1, 2)], [(0, 1), (1, 1),
+                                                                                                       (1, 2)],
+                        [(2, 0), (2, 1), (2, 2)], [(0, 2), (1, 2), (2, 2)], [(0, 0), (1, 1), (2, 2)], [(2, 0), (1, 1),
+                                                                                                       (0, 2)]]
+
 
 class CoordsTTTBoard:
     """Contains a blank TTT board as list of tuples items, and commands to modify the board, score game, and return str
@@ -45,12 +51,36 @@ class CoordsTTTBoard:
         >>> X.calc_winner()
         'X'
         """
+        coords_to_token = {}
+        key = itemgetter(0)
         for token in self._list_of_tokens:
-            if token[0]
-            
+            item = (token[2], (token[0], token[1]))
+            coords = (token[0], token[1])
+            group = key(item)
+            if group not in coords_to_token:
+                coords_to_token[group] = []
+            coords_to_token[group].append(coords)
+        for item in coords_to_token:
+            for combination in WINNING_COMBINATIONS:
+                if coords_to_token.values[item]() in combination:
+                    winner = item.keys()
+                else:
+                    winner = None
+        return winner
 
 
-    def __str__(self):
-        """Returns a pretty-printed string of the board.
+    # def __str__(self):
+    #     """Returns a pretty-printed string of the board.
+    #
+    #     """
 
-        """
+def group_by(iterable, key):
+    """Place each item in an iterable into a bucket based on calling the key
+    function on the item."""
+    group_to_items = {}
+    for item in iterable:
+        group = key(item)
+        if group not in group_to_items:
+            group_to_items[group] = []
+        group_to_items[group].append(item)
+    return group_to_items
