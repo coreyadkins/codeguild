781c560a57b6039e9e90838e507b5abc7450aa7d
Corey Adkins <adkinsbass@gmail.com>
2016-08-09 14:47:52 -0700

    Incorporates feedback

--- practice/caesar/site.js
+++ practice/caesar/site.js
@@ -1,65 +1,60 @@
 'use strict';
 
-var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+/**Returns the ASCII position of the inputted character.
+*/
+function getIndex(letter) {
+  return letter.charCodeAt();
+}
+
+/** Returns the ASCII character as a string associated with supplied index.
+*/
+function getLetter(index) {
+  return String.fromCharCode(index);
+}
 
 /** Takes in a string and shifts it [key] amount on the alphabet to encrypt
 the word.
 */
 function caesarEncrypt(key, inputWord) {
-  var inputWord = inputWord.toUpperCase();
-  var i = 0;
-  var outputArray = [];
-  for (var x = 0; x < inputWord.length; x += 1) {
-    var inputLetter = inputWord[x];
-    for (var y = 0; y < alphabet.length; y += 1) {
-      var letter = alphabet[y];
-      if (inputLetter === letter) {
-        var index = i + key;
-        if (index > 25) {
-          index -= 26;
-        }
-        outputArray.push(alphabet[index]);
-      }
-      if (i >= 25) {
-        i = 0;
-        if (inputLetter === ' ') {
-          outputArray.push(inputLetter);
-        }
-      } else {
-        i += 1;
-      }
+  var wordAsArray = inputWord.split('');
+  var indexArray = _.map(wordAsArray, getIndex);
+  var shiftedIndexArray = _.map(indexArray, function addKey(index) {
+    if (index === 32) {
+      return index;
+    } else {
+      var newIndex = index + key;
     }
-  }
+    if (newIndex > 122) {
+      newIndex -= 26;
+    } else if (newIndex > 90 && newIndex < 97) {
+      newIndex -= 26;
+    }
+    return newIndex;
+  });
+  var outputArray = _.map(shiftedIndexArray, getLetter);
   return outputArray.join('');
 }
 
-/**Takes in a string and shifts it back [key] amount on the alphabet to
-decrypt the word.
+
+/** Takes in a string and shifts it back [key] amount on the alphabet to decrypt
+the word.
 */
 function caesarDecrypt(key, inputWord) {
-  var inputWord = inputWord.toUpperCase();
-  var i = 0;
-  var outputArray = [];
-  for (var x = 0; x < inputWord.length; x += 1) {
-    var inputLetter = inputWord[x];
-    for (var y = 0; y < alphabet.length; y += 1) {
-      var letter = alphabet[y];
-      if (inputLetter === letter) {
-        var index = i - key;
-        if (index < 0) {
-          index += 26;
-        }
-        outputArray.push(alphabet[index]);
-      }
-      if (i >= 25) {
-        i = 0;
-        if (inputLetter === ' ') {
-          outputArray.push(inputLetter);
-        }
-      } else {
-        i += 1;
-      }
+  var wordAsArray = inputWord.split('');
+  var indexArray = _.map(wordAsArray, getIndex);
+  var shiftedIndexArray = _.map(indexArray, function addKey(index) {
+    if (index === 32) {
+      return index;
+    } else {
+      var newIndex = index - key;
     }
-  }
+    if (newIndex < 65) {
+      newIndex += 26;
+    } else if (newIndex > 90 && newIndex < 97) {
+      newIndex += 26;
+    }
+    return newIndex;
+  });
+  var outputArray = _.map(shiftedIndexArray, getLetter);
   return outputArray.join('');
 }
