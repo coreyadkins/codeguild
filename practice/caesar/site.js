d5c3f8608b3c3864fec150fe554a122c17bb80a6
Corey Adkins <adkinsbass@gmail.com>
2016-08-10 09:47:22 -0700

    Incorporates feedback

--- practice/caesar/site.js
+++ practice/caesar/site.js
@@ -1,33 +1,42 @@
 'use strict';
 
-/**Returns the ASCII position of the inputted character.
+/**
+ * Returns the ASCII position of the inputted character.
 */
 function getIndex(letter) {
   return letter.charCodeAt();
 }
 
-/** Returns the ASCII character as a string associated with supplied index.
+/**
+ * Returns the ASCII character as a string associated with supplied index.
 */
 function getLetter(index) {
   return String.fromCharCode(index);
 }
 
-/** Takes in a string and shifts it [key] amount on the alphabet to encrypt
-the word.
+/**
+ * Takes in a string and shifts it [key] amount on the alphabet to encrypt
+ * the word.
 */
+
 function caesarEncrypt(key, inputWord) {
   var wordAsArray = inputWord.split('');
   var indexArray = _.map(wordAsArray, getIndex);
-  var shiftedIndexArray = _.map(indexArray, function addKey(index) {
-    if (index === 32) {
+  var shiftedIndexArray = _.map(indexArray, function(index) {
+    var asciiSpace = 32;
+    if (index === asciiSpace) {
       return index;
     } else {
       var newIndex = index + key;
     }
-    if (newIndex > 122) {
-      newIndex -= 26;
-    } else if (newIndex > 90 && newIndex < 97) {
-      newIndex -= 26;
+    var asciiIndexZ = 122;
+    var alphabetsize = 26;
+    var asciiMiscCharMin = 91;
+    var asciiMiscCharMax = 96;
+    if (newIndex > asciiIndexZ) {
+      newIndex -= alphabetsize;
+    } else if (newIndex >= asciiMiscCharMin && newIndex <= asciiMiscCharMax) {
+      newIndex -= alphabetsize;
     }
     return newIndex;
   });
@@ -36,9 +45,11 @@ function caesarEncrypt(key, inputWord) {
 }
 
 
-/** Takes in a string and shifts it back [key] amount on the alphabet to decrypt
-the word.
-*/
+/**
+ * Takes in a string and shifts it back [key] amount on the alphabet to decrypt
+ * the word.
+ */
+
 function caesarDecrypt(key, inputWord) {
   var wordAsArray = inputWord.split('');
   var indexArray = _.map(wordAsArray, getIndex);
