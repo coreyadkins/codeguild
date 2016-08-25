3578881f38287ce9b7c5e2b5b59b9d0fa06b1fee
Corey Adkins <adkinsbass@gmail.com>
2016-08-25 11:53:04 -0700

    Refactors problem based on new parameters, incorporates feedback, includes docstrings on testable views.py functions

--- practice/timezone/timezone/logic.py
+++ practice/timezone/timezone/logic.py
@@ -12,10 +12,9 @@ def get_current_time():
 def get_timezone(lat, lng):
     """Inputs latitude and longitude coordinates, outputs the name of the timezone at those coordinates.
 
-    >>> get_timezone('43.068888', '-121.008911')
+    >>> get_timezone(43.068888, -121.008911)
     'America/Los_Angeles'
     """
-    lat, lng = float(lat), float(lng)
     tz = tzwhere.tzwhere()
     return tz.tzNameAt(lat, lng)
 
@@ -26,23 +25,15 @@ def get_time_at_timezone(timezone):
     return current_time.to(timezone).isoformat()
 
 
-def get_timezone_and_time(time, timezone):
-    """Takes a date and time and a timezone name, turns data into an arrow object representing this info.
+def convert_to_timezone(time_in, timezone_out):
+    """Takes a time and date, converts the time to the time at output timezone."
 
-    >>> get_timezone_and_time('2013-05-09T03:59:59.999999', 'America/New_York')
-    <Arrow [2013-05-09T03:59:59.999999-04:00]>
-    """
-    return arrow.get(time).replace(tzinfo=timezone)
-
-
-def convert_to_timezone(time_in_with_timezone, timezone_out):
-    """Takes an arrow time object, converts the time to the time at output timezone."
-
-    :param time_in_with_timezone: Arrow object of starting time
+    :param time_in: Starting time and date
     :param timezone_out: Timezone name of output timezone
     :return: Arrow object converted to new timezone
 
-    >>>convert_to_timezone(<Arrow [2013-05-09T03:59:59.999999-04:00]>, 'America/Los_Angeles')
+    >>> convert_to_timezone('2013-05-09T03:59:59.999999-04:00', 'America/Los_Angeles')
     <Arrow [2013-05-09T00:59:59.999999-07:00]>
     """
-    return time_in_with_timezone.to(timezone_out)
+    time_in = arrow.get(time_in)
+    return time_in.to(timezone_out)
--- practice/timezone/timezone/views.py
+++ practice/timezone/timezone/views.py
@@ -1,34 +1,40 @@
 """timezone Views."""
 
 from django.http import HttpResponse
-from . logic import get_current_time, get_timezone, get_time_at_timezone, get_timezone_and_time, convert_to_timezone
+from . import logic
 
 
 def get_server_time(request):
     """Retrieves the current time in UTC 8601 format."""
-    current_time = get_current_time()
+    current_time = logic.get_current_time()
     return HttpResponse(current_time)
 
 
 def get_timezone_at_lat_lng(request, lat, lng):
-    """Retrieves the timezone name at inputted longitude and latitude coordinates."""
-    timezone = get_timezone(lat, lng)
+    """Retrieves the timezone name at inputted longitude and latitude coordinates.
+
+    >>> X = get_timezone_at_lat_lng('request', '43.068888', '-121.008911')
+    >>> X.content
+    b'America/Los_Angeles'
+    """
+    timezone = logic.get_timezone(float(lat), float(lng))
     return HttpResponse(timezone)
 
 
 def get_time_at_lat_lng(request, lat, lng):
     """Retrives the current time at inputted longitude and latitude coordinates."""
-    timezone = get_timezone(lat, lng)
-    time_at_timezone = get_time_at_timezone(timezone)
+    timezone = logic.get_timezone(float(lat), float(lng))
+    time_at_timezone = logic.get_time_at_timezone(timezone)
     return HttpResponse(time_at_timezone)
 
 
-def convert_time_from_lat_lngs(request, in_lat, in_lng, in_time, out_lat, out_lng):
-    """Converts inputted time at inputted longitude and latitude coordinates into concurrent time at output
-    longitude and latitude coordinates.
+def convert_time_from_lat_lngs(request, in_time, out_lat, out_lng):
+    """Converts inputted time into concurrent time at output longitude and latitude coordinates.
+
+    >>> X = convert_time_from_lat_lngs('request', '2013-05-09T03:59:59.999999-04:00', '43.068888', '-121.008911')
+    >>> X.content
+    b'2013-05-09T00:59:59.999999-07:00'
     """
-    timezone_in = get_timezone(in_lat, in_lng)
-    time_in_with_timezone = get_timezone_and_time(in_time, timezone_in)
-    timezone_out = get_timezone(out_lat, out_lng)
-    converted_time = convert_to_timezone(time_in_with_timezone, timezone_out)
+    timezone_out = logic.get_timezone(float(out_lat), float(out_lng))
+    converted_time = logic.convert_to_timezone(in_time, timezone_out)
     return HttpResponse(converted_time)
