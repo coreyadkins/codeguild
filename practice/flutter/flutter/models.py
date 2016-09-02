07d84341d8adf36928b73c7604bd52b13156f8c9
Corey Adkins <adkinsbass@gmail.com>
2016-09-02 11:34:03 -0700

    Couple of PEP-8 fixes

--- practice/flutter/flutter/models.py
+++ practice/flutter/flutter/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 import datetime
 
+
 class Flutt(models.Model):
     """Flutt model stores a single Flutt, including its author, author id, the text or body of the Flutt, and a
     timestamp for the time of posting.
@@ -15,7 +16,8 @@ class Flutt(models.Model):
     def __str__(self):
         """Return str.
 
-        >>> str(Flutt(author='adkinsbass', author_id=0, body='Hi!', timestamp=datetime.datetime(2016, 9, 2, 10, 28, 35)))
+        >>> str(Flutt(author='adkinsbass', author_id=0, body='Hi!', timestamp=datetime.datetime(2016, 9, 2, 10, 28, 35)\
+        ))
         'Flutt(adkinsbass, Hi!)'
         """
         return 'Flutt({}, {})'.format(self.author, self.body)
@@ -27,5 +29,5 @@ class Flutt(models.Model):
                 )))
         "Flutt(author='adkinsbass', body='Hi!', timestamp=2016-09-02 10:28:35, author_id=0)"
         """
-        return 'Flutt(author={!r}, body={!r}, timestamp={}, author_id={})'.format(self.author, self.body, self.timestamp,
-                                                                                 self.author_id)
+        return 'Flutt(author={!r}, body={!r}, timestamp={}, author_id={})'.format(self.author, self.body,
+                                                                                  self.timestamp, self.author_id)
