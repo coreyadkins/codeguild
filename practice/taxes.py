afa12e43a7ca8b753bd1fd47fe9e410148085901
Corey Adkins <adkinsbass@gmail.com>
2017-04-20 16:28:59 -0500

    Working on inputs

--- practice/taxes.py
+++ practice/taxes.py
@@ -1,3 +1,23 @@
 """This program takes the user income and calculates and prints their total
-Oregon income tax.
+income tax.
 """
+# 1. Setup
+OREGON_BRACKETS = [(3350, 0.05), (5050, 0.07), (116000, 0.09), (None, 9.9)]
+
+# 2. input
+
+
+def input_tax_information():
+    """This gathers income and tax bracket information from the user."""
+    income = float(input('What is your taxable income for the year?'))
+    oregon_or_custom_bracket = str(input('Would you like to use the' +
+    'Oregon tax bracket or input a custom one? Please input "Oregon" or' +
+    '"Custom"'))
+    if oregon_or_custom_bracket == 'Custom':
+        is_progressive = str(input('Is this bracket progressive?'))
+    else:
+         tax_bracket = OREGON_BRACKETS
+    return [income, tax_bracket]
+# 3. Transform
+
+# 4. Output
